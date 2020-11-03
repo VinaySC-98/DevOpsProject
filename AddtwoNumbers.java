1ab7bd39143679b699c1a3b3ad75bf35334d2e7d
Chikkarudraiah <vinay.shivakote.chikkarudraiah@hp.com>
2020-11-03 16:15:37 +0530

    The addnewNumber java file is updated

---
+++ AddtwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddtwoNumbers{
+
+public static voie main(String args[]){
+
+int num1=15;
+int num2 =15;
+ int sum = num1+num2;
+System.out.println("The number is added "+sum);
+
+}
+
+}
